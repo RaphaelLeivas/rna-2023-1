424dbd5f617f8e866ca5b238fece2b70065ed41d
RaphaelLeivas <raphaelhenrique2013@outlook.com>
2023-03-24 14:32:50 -0300

    testes com funções plot e print

--- teste.R
+++ teste.R
@@ -1,3 +1,37 @@
-x <- 10
-print(10)
-plot(10)
+library(datasets)
+
+head(mtcars)
+
+print(table(mtcars$cyl))
+
+# plot(iris)
+
+# print(class(iris))
+
+# plot(
+#     iris$Petal.Length,
+#     iris$Petal.Width,
+#     col = "red",
+#     pch = 19,
+#     main = "Título do gráfico 01",
+#     xlab = "Eixo X",
+#     ylab = "Eixo Y",
+# )
+
+# dev.off()
+
+
+plot(
+    cos,
+    -30, +30,
+    col = "black",
+    pch = 19,
+    main = "Título do gráfico 02",
+    xlab = "Eixo X",
+    ylab = "Eixo Y",
+    lwd = 3
+)
+
+# x <- 10
+# print(10)
+# plot(10)
